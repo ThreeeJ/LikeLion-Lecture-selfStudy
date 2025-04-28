5ed9bd31a3e4f8fc95bf7b84fd1882a0e993e152
ThreeeJ <2171266@hansung.ac.kr>
2025-04-28 17:00:13 +0900

    [섹션 3] 4. HTTP와 String

--- lecture3/src/main/java/com/ll/lecture3/HomeController.java
+++ lecture3/src/main/java/com/ll/lecture3/HomeController.java
@@ -2,8 +2,8 @@ package com.ll.lecture3;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 // 해당 클래스의 객체를
 // 스프링부트가 생성하고(1개만)
@@ -13,8 +13,11 @@ public class HomeController {
 
     @GetMapping("a")
     @ResponseBody
-    public String hello() {
-        return "Hello";
+    public String hello(
+            String age,
+            String id
+    ) {
+        return "%s번 사람의 나이는 %s살 입니다.".formatted(age, id);
     }
 
     // 해당 메서드는 브라우저를 통해 호출 가능해집니다.
@@ -26,7 +29,18 @@ public class HomeController {
 
     @GetMapping("c")
     @ResponseBody // 함수의 리턴값이 브라우저에 보여진다.
-    public String hello3() {
-        return "안녕하세요.";
+    // http://localhost:8090/c?a=10&b=20
+    // a의 값(10)이 num1Str에 매핑되고, b의 값(20)이 num2Str에 매핑된다.
+    public String plus(
+            @RequestParam("a") String num1Str,
+            @RequestParam("b") String num2Str
+    ) {
+        int num1 = Integer.parseInt(num1Str);
+        int num2 = Integer.parseInt(num2Str);
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        return "a + b = %d".formatted(num1 + num2);
     }
 }
