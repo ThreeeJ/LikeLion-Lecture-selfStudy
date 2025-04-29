2baacc853644a64242c1034c2ffc1c63fb14eaaa
ThreeeJ <2171266@hansung.ac.kr>
2025-04-29 20:33:59 +0900

    [섹션 3] 8. 할 일 조회

--- lecture3-7_9/src/main/java/com/ll/lecture3_7_9/TodoController.java
+++ lecture3-7_9/src/main/java/com/ll/lecture3_7_9/TodoController.java
@@ -1,6 +1,7 @@
 package com.ll.lecture3_7_9;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,40 @@ public class TodoController {
         todos = new ArrayList<>();
     }
 
-    @GetMapping("/add")
+    @GetMapping("") // 조회
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    @GetMapping("/detail")
+    public Todo getTodo1(
+            long id
+    ) {
+        return todos
+                .stream()
+                .filter(
+                        todo -> todo.getId() == id
+                )
+                .findFirst()
+                .orElse(null);
+    }
+
+    @GetMapping("/{id}") // {} 감싸져 있으면 어떤 변수가 올지 모른다.
+    // 주소 자체에 숫자를 넣으면 "id=숫자" 로 인식
+    public Todo getTodo2(
+            @PathVariable long id // 주소에 적은 숫자값이 여기에 들어간다.
+            // 그렇게 하기 위해서는, @PathVariable 이거 붙여야 한다.
+    ) {
+        return todos
+                .stream()
+                .filter(
+                        todo -> todo.getId() == id
+                )
+                .findFirst()
+                .orElse(null);
+    }
+
+    @GetMapping("/add") // 추가
     public Todo add(
             String body
     ) {
