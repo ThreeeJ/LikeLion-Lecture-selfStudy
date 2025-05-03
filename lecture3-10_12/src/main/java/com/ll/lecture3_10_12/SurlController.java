cb03614637965f97565bd150d4984945a746765c
ThreeeJ <2171266@hansung.ac.kr>
2025-05-03 17:57:46 +0900

    [섹션 3] 11. URL 단축방식 개선

--- lecture3-10_12/src/main/java/com/ll/lecture3_10_12/SurlController.java
+++ lecture3-10_12/src/main/java/com/ll/lecture3_10_12/SurlController.java
@@ -1,10 +1,13 @@
 package com.ll.lecture3_10_12;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -16,6 +19,39 @@ public class SurlController {
     @GetMapping("/add")
     @ResponseBody
     public Surl add(String body, String url) {
+        Surl surl = Surl
+                .builder()
+                .id(++surlsLastId)
+                .body(body)
+                .url(url)
+                .build();
+
+        surls.add(surl);
+
+        return surl;
+    }
+
+    @GetMapping("/s/{body}/**")
+    @ResponseBody
+    public Surl add(
+            @PathVariable String body,
+            HttpServletRequest req
+    ) {
+        String url = req.getRequestURI();
+        // ? 전까지만 나옴
+        // "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EB%84%A4%EC%9D%B4%EB%B2%84&ackey=lewc2xbi"
+        // "https://search.naver.com/search.naver" : getRequestURI() 하면 '?' 전까지만 나옴
+
+        if (req.getQueryString() != null) {
+            url = url + "?" + req.getQueryString();
+        }
+
+        String[] urlBits = url.split("/", 4);
+
+        System.out.println("Arrays.toString(urlBits): " + Arrays.toString(urlBits));
+
+        url = urlBits[3];
+
         Surl surl = Surl
                 .builder()
                 .id(++surlsLastId)
